057b20bb55679c47c1cf984ef708456904236882
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-10-12 20:00:20 -1000

    Add Frame

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
                 compilerOptions: {
 
                     isCustomElement: (tag) => {return tag.endsWith('component')},
+
                 }
             }
         }),
