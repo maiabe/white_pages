59ce8fe6d41c87aa646bdf6c397d6c3ba0d8bce4
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-10-21 12:33:49 -1000

    edit the alert bar messages

--- app/Http/Controllers/DeptGroupController.php
+++ app/Http/Controllers/DeptGroupController.php
@@ -2,25 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use http\Message;
 use Illuminate\Http\Request;
 use App\Models\DeptGroup;
 use App\Models\Campus;
 
 class DeptGroupController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $data = DeptGroup::all();
         $campusData = Campus::distinct()->pluck('campus_code');
         return view('dept_group', ['data' => $data, 'campusData' => $campusData]);
     }
 
-    public function destroy($dept_grp,){
-        DeptGroup::where('dept_grp',$dept_grp)->delete();
+    public function destroy($dept_grp,)
+    {
+        DeptGroup::where('dept_grp', $dept_grp)->delete();
         return redirect()->route('dept_group.index');
     }
 
-    public function update(Request $req, $dept_grp) {
+    public function update(Request $req, $dept_grp)
+    {
         $deptGroup = DeptGroup::where('dept_grp', $dept_grp)->first();
+
+        $messages = [
+            'dept_grp.unique' => 'The department group: ' . $req->dept_grp . ' is already in use. Fail to update for the department group: ' . $deptGroup->dept_grp . ".",
+            'dept_grp_name.unique' => 'The department group name:' . $req->dept_grp_name . 'is already in use. Fail to update for the department group name: ' . $deptGroup->dept_grp_name . ".",
+        ];
+
         // Define validation rules
         $validatedData = $req->validate([
             'dept_grp' => [
@@ -36,7 +46,7 @@ class DeptGroupController extends Controller
                 'unique:dept_group,dept_grp_name,' . $deptGroup->dept_grp_name . ',dept_grp_name'
             ],
             'campus_code' => 'required|string|max:6',
-        ]);
+        ], $messages);
 
         // Attempt to update the record
         DeptGroup::where('dept_grp', $dept_grp)->update([
--- resources/views/dept_group.blade.php
+++ resources/views/dept_group.blade.php
@@ -44,11 +44,15 @@
     <br>
     @if(count($data)>0)
     @if ($errors->any())
-    @foreach ($errors->all() as $error)
-    <h5 class="alert alert-danger">The Department Grouping does not updated successfully</h5>
-    @endforeach
+    <h6 class="alert alert-danger">
+        The Department Grouping was not updated successfully<br>
+        @foreach ($errors->all() as $error)
+        &#9888; {{ $error }}<br>
+        @endforeach
+        Try to re-updated!
+    </h6>
     @endif
-    <button type = "button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addDeptGrpModal">
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addDeptGrpModal">
         Add Department Group
     </button>
     <table id="table" class="table table-hover">
@@ -140,17 +144,11 @@
                             <label for="edit-dept-grp">Group Number</label>
                             <input type="text" name="dept_grp" class="form-control" id="edit-dept-grp"
                                    required minlength="6" maxlength="6">
-                            @error('dept_grp')
-                            <span class="text-danger" id="edit-dept-grp-error">{{$message}}</span>
-                            @enderror
                         </div>
                         <div class="form-group">
                             <label for="edit-dept-grp-name">Department Group Name</label>
                             <input type="text" name="dept_grp_name" class="form-control" id="edit-dept-grp-name"
-                                required minlength="3" maxlength="60">
-                            @error('dept_grp_name')
-                            <span class="text-danger" id="edit-dept-grp-name-error">{{$message}}</span>
-                            @enderror
+                                   required minlength="3" maxlength="60">
                         </div>
                         <div class="form-group">
                             <label for="edit-dept-grp-name">Campus Code</label>
@@ -209,34 +207,21 @@
             deleteUrl = deleteUrl.replace(":deptGrp", deptGrp);
             $("#delete-form").attr("action", deleteUrl);
         });
-        $(document).ready(function () {
-            $("#edit-close-button").on("click", function () {
-                // Clear any existing error messages
-                $("#edit-dept-grp-error").text("");
-                $("#edit-dept-grp-name-error").text("");
-            });
+        // Function to handle the delete button click
+        $(".edit-button").on("click", function () {
+            var deptGrp = $(this).data("dept-grp");
+            var deptGrpName = $(this).data("dept-grp-name");
+            var campusCode = $(this).data("campus-code");
 
-            $("#edit-x-button").on("click", function () {
-                // Clear any existing error messages
-                $("#edit-dept-grp-error").text("");
-                $("#edit-dept-grp-name-error").text("");
-            });
+            $("#edit-dept-grp").val(deptGrp);
+            $("#edit-dept-grp-name").val(deptGrpName);
+            $("#edit-campus-code").val(campusCode);
+            $("#editModal").modal("show");
 
-
-            $(".edit-button").on("click", function () {
-                var deptGrp = $(this).data("dept-grp");
-                var deptGrpName = $(this).data("dept-grp-name");
-                var campusCode = $(this).data("campus-code");
-
-                $("#edit-dept-grp").val(deptGrp);
-                $("#edit-dept-grp-name").val(deptGrpName);
-                $("#edit-campus-code").val(campusCode);
-                $("#editModal").modal("show");
-
-                var editUrl = "{{ route('dept_group.update', ':deptGrp') }}";
-                editUrl = editUrl.replace(":deptGrp", deptGrp);
-                $("#editModal form").attr("action", editUrl);
-            });
+            // Update the form action with the correct URL
+            var editUrl = "{{ route('dept_group.update', ':deptGrp') }}";
+            editUrl = editUrl.replace(":deptGrp", deptGrp);
+            $("#editModal form").attr("action", editUrl);
         });
 
     });
