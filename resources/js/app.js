90a26c2083112543ece5a2f9e254822f0e2b5fe5
maiabe <maiabe@hawaii.edu>
2023-10-26 11:57:51 -1000

    edit app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+//import './bootstrap.js';
 import { createApp } from 'vue';
 import NavbarComponent from './components/NavbarComponent.vue';
 import SidebarComponent from './components/SidebarComponent.vue';
