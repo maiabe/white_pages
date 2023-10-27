511e36eec27b12b232c6c3ff129969d329fe622b
Tracy Bui <89670935+tracytnb@users.noreply.github.com>
2023-10-26 20:38:59 -0500

    Merge footer design into main (#83)
    * edited footer component to reflect hawaii.edu site
    
    * Update FooterComponent.vue
    
    * changed size of last column
    
    ---------
    
    Co-authored-by: FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,159 +1,159 @@
 <template>
-    <div>
-        <footer class="text-center text-lg-start text-white" style="background-color: #222222">
-            <!-- Grid container START-->
-            <div class="container p-4 pb-0">
-                <!-- SECTION: Footer -->
-                <section class="">
-                    <!--Grid row: Upper Row -->
-                    <div class="row">
-                        <!-- Grid column -->
-                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                            <a href="#">
-                                <img src="/images/white-seal.png" alt="University of Hawaii System seal and name" style="width: 170px; height: 60px;">
-                            </a>
-                            <p style="margin: 0;">
-                                2444 Dole Street
-                            </p>
-                            <p style="margin: 0;">
-                                Honolulu, HI 96822
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <hr class="w-100 clearfix d-md-none"/>
-
-                        <!-- Grid column -->
-                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                            <p style="margin: 0;">
-                                An
-                                <a class="dotted-links" href="#">equal opportunity/ </a>
-                                <a class="dotted-links" href="#">affirmative action institution</a>
-                            </p>
-                            <p style="margin: 0;">
-                                Use of this site implies consent with our
-                                <a class="dotted-links" href="#"> Usage Policy</a>
-                                copyright © 2023
-                                <a class="dotted-links" href="#">University of Hawaiʻi</a>
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <hr class="w-100 clearfix d-md-none"/>
-
-                        <!-- Grid column -->
-                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">Calendar</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">COVID-19 Updates</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">Directory</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">Emergency information</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">MyUH</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">Privacy Statement</a>
-                            </p>
-                            <p style="margin: 0;">
-                                <a class="non-dotted-links">Work at UH</a>
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-
-                        <hr class="w-100 clearfix d-md-none"/>
-
-                        <!-- Grid column -->
-                        <div class="col-md-4 col-lg-4 col-x1-2 mx-auto mt-3">
-                            <div class="link-container">
-                                <p><i class="fas fa-twitter"></i></p>
-                                <p><i class="fas fa-twitter"></i></p>
-                                <p><i class="fas fa-twitter"></i></p>
-                                <p><i class="fas fa-twitter"></i></p>
-                                <p><i class="fas fa-twitter"></i></p>
-                                <p><i class="fas fa-twitter"></i></p>
-                            </div>
-                            <div class="column 4">
-                                <p class="font-weight-bold">
-                                    <a class="dotted-links" href="#">Contact UH</a>
-                                </p>
-                                <p>
-                                    If required, information contained on this website can be made available in an alternative format upon request.
-                                </p>
-                            </div>
-                            <p>
-                                <a class="dotted-links" href="#">Get Adobe Acrobat Reeader</a>
-                            </p>
-                        </div>
-                        <!-- Grid column -->
-                    </div>
-                    <!--Grid row: Upper Row-->
-                </section>
-                <!-- Section: Links -->
-
-                <!-- SECTION: Languages -->
-                <section class="">
-                    <div class="row d-flex align-items-center" style="padding-top: 25px;">
-                        <!-- Language Forms -->
-                        <div class="link-container">
-                            <p>
-                                <a class="dotted-links" href="#">English</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Gagana Samoa</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Kapasen Chuuk</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Tiếng Việt</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">ภาษาไทย</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Ilokano</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Tagalog</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Cebuano</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Kajin Majôl</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">简体中文</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">繁體中文</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">日本語</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">한국어</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">Español</a>
-                            </p>
-                            <p>
-                                <a class="dotted-links" href="#">ʻŌlelo Hawaiʻi</a>
-                            </p>
-                        </div>
-                    </div>
-                </section>
-                <!-- SECTION: Footer -->
+  <div>
+    <footer class="text-center text-lg-start text-white" style="background-color: #222222">
+      <!-- Grid container START-->
+      <div class="container p-4 pb-0">
+        <!-- SECTION: Footer -->
+        <section class="">
+          <!--Grid row: Upper Row -->
+          <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <a href="#">
+              <img src="/images/white-seal.png" alt="University of Hawaii System seal and name" style="width: 170px; height: 60px;">
+              </a>
+              <p style="margin: 0;">
+                2444 Dole Street
+              </p>
+              <p style="margin: 0;">
+              Honolulu, HI 96822
+              </p>
             </div>
-        </footer>
-    </div>
+            <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none"/>
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <p style="margin: 0;">
+                An
+                <a class="dotted-links" href="#">equal opportunity/ </a>
+                <a class="dotted-links" href="#">affirmative action institution</a>
+              </p>
+              <p style="margin: 0;">
+                Use of this site implies consent with our
+                <a class="dotted-links" href="#"> Usage Policy</a>
+                copyright © 2023
+                <a class="dotted-links" href="#">University of Hawaiʻi</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+                        <hr class="w-100 clearfix d-md-none"/>
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <p style="margin: 0;">
+                <a class="non-dotted-links">Calendar</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">COVID-19 Updates</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">Directory</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">Emergency information</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">MyUH</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">Privacy Statement</a>
+              </p>
+              <p style="margin: 0;">
+                <a class="non-dotted-links">Work at UH</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <hr class="w-100 clearfix d-md-none"/>
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-3 col-x1-2 mx-auto mt-3">
+              <div class="link-container">
+                <p><i class="fas fa-twitter"></i></p>
+                <p><i class="fas fa-twitter"></i></p>
+                <p><i class="fas fa-twitter"></i></p>
+                <p><i class="fas fa-twitter"></i></p>
+                <p><i class="fas fa-twitter"></i></p>
+                <p><i class="fas fa-twitter"></i></p>
+              </div>
+              <div class="column 4">
+                <p class="font-weight-bold">
+                  <a class="dotted-links" href="#">Contact UH</a>
+                </p>
+                <p>
+                If required, information contained on this website can be made available in an alternative format upon request.
+                </p>
+              </div>
+              <p>
+                  <a class="dotted-links" href="#">Get Adobe Acrobat Reeader</a>
+                </p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!--Grid row: Upper Row-->
+        </section>
+        <!-- Section: Links -->
+
+        <!-- SECTION: Languages -->
+        <section class="">
+          <div class="row d-flex align-items-center" style="padding-top: 25px;">
+              <!-- Language Forms -->
+              <div class="link-container">
+                <p>
+                  <a class="dotted-links" href="#">English</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Gagana Samoa</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Kapasen Chuuk</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Tiếng Việt</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">ภาษาไทย</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Ilokano</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Tagalog</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Cebuano</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Kajin Majôl</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">简体中文</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">繁體中文</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">日本語</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">한국어</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">Español</a>
+                </p>
+                <p>
+                  <a class="dotted-links" href="#">ʻŌlelo Hawaiʻi</a>
+                </p>
+              </div>
+          </div>
+        </section>
+        <!-- SECTION: Footer -->
+      </div>
+    </footer>
+  </div>
 </template>
 <style scoped src="../resources/css/app.css">
 {
