0450afb097ef4b930a5fb387bd010a4e26e53dde
maia <31334240+maiabe@users.noreply.github.com>
2023-10-26 23:11:09 -1000

    Dev mai 01 (#84)
    * Add file structures
    
    *Add sidebar
    
    * merge vite node modules
    
    * Merge all contributions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue';
-import HeaderComponent from './components/HeaderComponent.vue';
-import SideBar from './components/SideBar.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
+import SidebarComponent from './components/SidebarComponent.vue';
 import FooterComponent from './components/FooterComponent.vue';
+//-- Import Font Awesome
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas);
 
-const appHeader = createApp(HeaderComponent);
-const appSideBar = createApp(SideBar);
-const appFooter = createApp(FooterComponent);
+const app = createApp({});
 
-appHeader.mount('#header'); // Mount the HeaderComponent
-appSideBar.mount('#sidebar'); // Mount the SideBar
-appFooter.mount('#footer'); // Mount the FooterComponent
+app
+    .component('NavbarComponent', NavbarComponent)
+    .component('SidebarComponent', SidebarComponent)
+    .component('FooterComponent', FooterComponent)
+    .component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
---
+++ resources/views/Layout/Partials/sidebar.blade.php
@@ -0,0 +1,10 @@
+<!-- <sidebar-component></sidebar-component> -->
+<sidebar-component
+    :person-listings-route="'{{ route('person_listings') }}'"
+    :department-listings-route="'{{ route('department_listings') }}'"
+    :dept-groups-route="'{{ route('dept_groups') }}'"
+    :dept-contacts-route="'{{ route('dept_contacts') }}'"
+    :announcements-route="'{{ route('announcements') }}'"
+    :admins-route="'{{ route('admins') }}'"
+    >
+<sidebar-component/>
