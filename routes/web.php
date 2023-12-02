d6b251fb0af97e783aefe321d5754b3679f0d927
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-12-02 09:13:55 -1000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,28 +25,6 @@ use App\Http\Controllers\ProductController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/campus',[CampusController::class, 'index']);
-Route::get('/departments','App\Http\Controllers\DepartmentController@index')->name('departments.index');
-
-Route::get('/dept_groups',[DeptGroupController::class, 'index'])->name('dept_groups');
-Route::delete('dept_groups/{dept_grp}','App\Http\Controllers\DeptGroupController@destroy')->name('dept_groups.destroy');
-Route::put('dept_groups/{dept_grp}','App\Http\Controllers\DeptGroupController@update')->name('dept_groups.update');
-Route::post('dept_groups', [DeptGroupController::class, 'store'])->name('dept_groups.store');
-
-Route::get('/person_listings',[PersonController::class, 'index'])->name('person_listings');
-Route::delete('person_listings/{username}', 'App\Http\Controllers\PersonController@destroy')->name('person_listings.destroy');
-Route::put('person_listings/{username}', 'App\Http\Controllers\PersonController@update')->name('person_listings.update');
-Route::post('person_listings', [PersonController::class, 'store'])->name('person_listings.store');
-
-//Route::get('/dept_groups',[DeptGroupController::class, 'index'])->name('dept_groups');
-Route::get('/dept_contacts',[DeptContactController::class, 'index'])->name('dept_contacts');
-Route::get('/department_listings',[DepartmentController::class, 'index'])->name('department_listings');
-Route::get('/announcements',[AnnouncementController::class, 'index'])->name('announcements');
-Route::get('/admins',[AdminController::class, 'index'])->name('admins');
-
-
-Route::get('/profile',[ProfileController::class, 'index'])->name('profile');
-
 Auth::routes();
 
 
@@ -54,15 +32,36 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-Route::post('/logout', function () {
-    Auth::logout();
-    return redirect('/login');
-})->name('logout');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::group(['middleware' => ['auth']], function() {
+    Route::post('/logout', function () {
+        Auth::logout();
+        return redirect('/login');
+    })->name('logout');
+
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/campus',[CampusController::class, 'index']);
+    Route::get('/departments','App\Http\Controllers\DepartmentController@index')->name('departments.index');
+
+    Route::get('/dept_groups',[DeptGroupController::class, 'index'])->name('dept_groups');
+    Route::delete('dept_groups/{dept_grp}','App\Http\Controllers\DeptGroupController@destroy')->name('dept_groups.destroy');
+    Route::put('dept_groups/{dept_grp}','App\Http\Controllers\DeptGroupController@update')->name('dept_groups.update');
+    Route::post('dept_groups', [DeptGroupController::class, 'store'])->name('dept_groups.store');
+
+    Route::get('/person_listings',[PersonController::class, 'index'])->name('person_listings');
+    Route::delete('person_listings/{username}', 'App\Http\Controllers\PersonController@destroy')->name('person_listings.destroy');
+    Route::put('person_listings/{username}', 'App\Http\Controllers\PersonController@update')->name('person_listings.update');
+    Route::post('person_listings', [PersonController::class, 'store'])->name('person_listings.store');
+
+    //Route::get('/dept_groups',[DeptGroupController::class, 'index'])->name('dept_groups');
+    Route::get('/dept_contacts',[DeptContactController::class, 'index'])->name('dept_contacts');
+    Route::get('/department_listings',[DepartmentController::class, 'index'])->name('department_listings');
+    Route::get('/announcements',[AnnouncementController::class, 'index'])->name('announcements');
+    Route::get('/admins',[AdminController::class, 'index'])->name('admins');
+
+
+    Route::get('/profile',[ProfileController::class, 'index'])->name('profile');
+
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
-    Route::resource('products', ProductController::class);
 });
