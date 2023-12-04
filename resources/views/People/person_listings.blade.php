73d0f5c0257469509aab6374eb8ccaee3b38f72c
maiabe <maiabe@hawaii.edu>
2023-12-03 23:29:19 -1000

    in progress person table

--- resources/views/People/person_listings.blade.php
+++ resources/views/People/person_listings.blade.php
@@ -10,11 +10,7 @@
     </div>
 
     <div class="main-container">
-        <div class="add-button-wrapper">
-            <button id="custom-add-btn" class="add-button" type="button" data-bs-toggle="modal" data-bs-target="#addPersonModal">
-                Add New Person
-            </button>
-        </div>
+        
         @if ($errors->any())
             <h6 class="alert alert-danger mt-4">
                 <strong>The Person was not updated successfully</strong>
@@ -28,6 +24,102 @@
                 Please revise and resubmit to update person record!
             </h6>
         @endif
+
+
+        <!-- Add Person Button -->
+        <div class="add-button-wrapper">
+            <button id="custom-add-btn" class="add-button" type="button" data-bs-toggle="modal" data-bs-target="#addPersonModal">
+                Add New Person
+            </button>
+        </div>
+        <!-- Add Modal -->
+        <div class="modal fade" id="addPersonModal" tabindex="-1" aria-labelledby="addPersonModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addPersonModalLabel">Add New Person</h5>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"
+                                id="add-x-button">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- Form for adding a new Person -->
+                        <form action="{{route('person_listings.store')}}" method="POST" id="addPersonForm">
+                            @csrf
+                            <div class="form-group">
+                                <label for="add-username">Username</label>
+                                <input type="text" class="form-control" id="username" name="username"
+                                    required minlength="2" maxlength="60" title="Enter a username (2 to 60 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name"
+                                    required minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-name-of-record">Name of Record</label>
+                                <input type="text" class="form-control" id="name_of_record" name="name_of_record"
+                                    required minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-job-title">Job Title</label>
+                                <input type="text" class="form-control" id="job_title" name="job_title"
+                                        minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-email">Email</label>
+                                <input type="text" class="form-control" id="email" name="email"
+                                        required pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Enter a valid email address (e.g., example@hawaii.edu)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-alias-email">Alias Email</label>
+                                <input type="text" class="form-control" id="alias_email" name="alias_email"
+                                        pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" maxlength="100" title="Enter a valid alias email (10 to 100 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-phone">Phone</label>
+                                <input type="text" class="form-control" id="phone" name="phone"
+                                    required pattern="^\d{3}-\d{3}-\d{4}$" title="Enter a phone number in the format xxx-xxx-xxxx (e.g., 123-456-7890)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-location">Location</label>
+                                <input type="text" class="form-control" id="location" name="location"
+                                    required minlength="2" maxlength="100" title="Enter a name (2 to 100 characters)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-fax">Fax</label>
+                                <input type="text" class="form-control" id="fax" name="fax"
+                                    pattern="^\+?[0-9]+(\s?[-.]?\s?[0-9]+)*$" title="Enter a valid fax number, e.g., +1 808-123-4567">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-website">Website</label>
+                                <input type="text" class="form-control" id="website" name="website"
+                                        pattern="/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;"
+                                        maxlength="200" title="Enter a valid website URL (e.g., http://hawaii.edu)">
+                            </div>
+                            <div class="form-group">
+                                <label for="add-publishable">Publishable</label>
+                                <select name="publishable" class="form-control" id="publishable">
+                                    <option value="true">True</option>
+                                    <option value="false">False</option>
+                                </select>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                                        id="add-close-button">
+                                    Close
+                                </button>
+                                <button type="submit" class="btn btn-primary">Add</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <nav>
             <div class="nav nav-pills nav-tabs" id="nav-tab" role="tablist">
                 <button class="nav-link active" id="nav-person-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Persons</button>
@@ -414,92 +506,7 @@
         </div>
     </div>
 </div>
-<!-- Add Modal -->
-<div class="modal fade" id="addPersonModal" tabindex="-1" aria-labelledby="addPersonModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="addPersonModalLabel">Add New Person</h5>
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"
-                        id="add-x-button">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Form for adding a new Person -->
-                <form action="{{route('person_listings.store')}}" method="POST" id="addPersonForm">
-                    @csrf
-                    <div class="form-group">
-                        <label for="add-username">Username</label>
-                        <input type="text" class="form-control" id="username" name="username"
-                               required minlength="2" maxlength="60" title="Enter a username (2 to 60 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name"
-                               required minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-name-of-record">Name of Record</label>
-                        <input type="text" class="form-control" id="name_of_record" name="name_of_record"
-                               required minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-job-title">Job Title</label>
-                        <input type="text" class="form-control" id="job_title" name="job_title"
-                                minlength="2" maxlength="255" title="Enter a name (2 to 255 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-email">Email</label>
-                        <input type="text" class="form-control" id="email" name="email"
-                                required pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" title="Enter a valid email address (e.g., example@hawaii.edu)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-alias-email">Alias Email</label>
-                        <input type="text" class="form-control" id="alias_email" name="alias_email"
-                                pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" maxlength="100" title="Enter a valid alias email (10 to 100 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-phone">Phone</label>
-                        <input type="text" class="form-control" id="phone" name="phone"
-                               required pattern="^\d{3}-\d{3}-\d{4}$" title="Enter a phone number in the format xxx-xxx-xxxx (e.g., 123-456-7890)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-location">Location</label>
-                        <input type="text" class="form-control" id="location" name="location"
-                               required minlength="2" maxlength="100" title="Enter a name (2 to 100 characters)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-fax">Fax</label>
-                        <input type="text" class="form-control" id="fax" name="fax"
-                               pattern="^\+?[0-9]+(\s?[-.]?\s?[0-9]+)*$" title="Enter a valid fax number, e.g., +1 808-123-4567">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-website">Website</label>
-                        <input type="text" class="form-control" id="website" name="website"
-                                pattern="/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g;"
-                                maxlength="200" title="Enter a valid website URL (e.g., http://hawaii.edu)">
-                    </div>
-                    <div class="form-group">
-                        <label for="add-publishable">Publishable</label>
-                        <select name="publishable" class="form-control" id="publishable">
-                            <option value="true">True</option>
-                            <option value="false">False</option>
-                        </select>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
-                                id="add-close-button">
-                            Close
-                        </button>
-                        <button type="submit" class="btn btn-primary">Add</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!-- Approve Modal with Editing -->
 <div class="modal fade" id="approvePersonModal" tabindex="-1" role="dialog" aria-labelledby="approveModalLabel"
      aria-hidden="true">
