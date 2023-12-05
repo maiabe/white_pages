a6a062a9f2d66193b70210704f9a8fc83e097dd5
FeimeiChen <54688836+FeimeiChen@users.noreply.github.com>
2023-12-04 23:08:46 -1000

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,30 @@
                @endif
             @endif
         </div>
+        <div class="main">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+                    <h2 class="text-center">White Pages Management System </h2>
+                    <p class="text-center">This is a brief description of the website. You can add any relevant information or text here.</p>
+                </div>
+                <div class="col-md-6 mx-auto">
+                    <img src="/images/logo/WPS.png" alt="UH White Page" class="navbar-logo img-fluid">
+                </div>
+            </div>
+            <div class="row section-2">
+                <div class="col-md-6 ">
+                    <div class="user-welcome text-center">
+                        <div class="user-icon-container">
+                            <font-awesome-icon icon="user" style="height: 100px; width: 100px; color: #0c0c0c;"></font-awesome-icon>
+                        </div>
+                        <div class="welcome-text">
+                            <h2>Welcome</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 
